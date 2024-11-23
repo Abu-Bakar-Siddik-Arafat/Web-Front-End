23b9331d180bb6e3448beb08caa80c359129c8fd
Abu-Bakar-Siddik-Arafat <abu15-13809@diu.edu.bd>
2024-11-23 12:44:32 +0600

    final changes
    Successfully added reset button save button and modified them accordingly

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,11 @@
 let countEl= document.getElementById("count-el")
 let toEl=document.getElementById("total-el")
 let welcomeEl=document.getElementById("welcome-el")
+let resetEl=document.getElementById("resetMgs-el")
 let count=0
 let dash = " - "
 let mgs="Welcome!!!"
+let resetMgs="Reset Successfully"
 console.log(mgs)
 welcomeEl.innerText=mgs
 function welcomeMgs(){
@@ -25,9 +27,13 @@ function save(){
     let tCounter =" " + count + dash
     toEl.innerText+= tCounter
     console.log(tCounter)
+    count=0
+    countEl.textContent=count
+    resetEl.innerText=""
 }
 
 function reset(){
     count = 0
     countEl.innerText=count
+    resetEl.textContent=resetMgs
 }
