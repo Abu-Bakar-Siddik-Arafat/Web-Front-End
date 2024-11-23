a9e2db5705e60ef5fa64d6d6a8824049812927c4
Abu-Bakar-Siddik-Arafat <abu15-13809@diu.edu.bd>
2024-11-23 12:32:15 +0600

    practice

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,9 @@
-let countEl= document.getElementById("count-el")
-let toEl=document.getElementById("total-el")
-let count=0
-let tl=7
-function increment(){
-    count+=1
-    console.log(count)
-    countEl.innerText=count
-}
+let firstName="Raiyan"
+let lastName="Arafat"
+let fulName=firstName + " "+lastName
 
-function decrement(){
-    count-=1
-    console.log(count)
-    countEl.innerText=count
-}
+console.log(fulName)
 
-function save(){
+function name(){
     
-    console.log(count)
-    toEl.innerText=count
-}
-
-function reset(){
-    count = 0
-    countEl.innerText=count
 }
