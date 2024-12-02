65a09f1b571521ea21c3b37df40afbb40d0f3be5
Abu-Bakar-Siddik-Arafat <abu15-13809@diu.edu.bd>
2024-12-02 18:08:32 +0600

    Calculator app

---
+++ JS/Calculator/index.js
@@ -0,0 +1,11 @@
+let displayEl=document.getElementById("display-el")
+
+let str="Display"
+console.log(str)
+/* displayEl.innerText=str */
+
+function add(){
+    
+    console.log(str)
+    displayEl.textContent=str
+}
